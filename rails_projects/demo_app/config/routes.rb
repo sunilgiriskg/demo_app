ecdf1ef3d137d73bd259543d065bce1ed02fd407
sunilgiri <sunilgiri@ubuntu.ubuntu-domain>
2014-07-18 12:15:50 +0530

    Finish demo  app

--- rails_projects/demo_app/config/routes.rb
+++ rails_projects/demo_app/config/routes.rb
@@ -1,4 +1,10 @@
 DemoApp::Application.routes.draw do
+  resources :microposts
+
+
+  resources :users
+
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -49,7 +55,7 @@ DemoApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
-
+    root :to => 'users#index'
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
